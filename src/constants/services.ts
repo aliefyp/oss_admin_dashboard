079949b7dc53cc22e480ea3061791889e15c4438
Alief Yoga Priyanto <aliefyp@gmail.com>
2024-03-17 16:29:25 +0700

    feat: update services

--- src/constants/services.ts
+++ src/constants/services.ts
@@ -9,26 +9,26 @@ const services = [
     name: 'Driving License',
     icon: '/icon_service_driving_license.svg'
   },
-  {
-    id: 'nic',
-    name: 'NIC',
-    icon: '/icon_service_nic.svg'
-  },
+  // {
+  //   id: 'nic',
+  //   name: 'NIC',
+  //   icon: '/icon_service_nic.svg'
+  // },
   {
     id: 'family-card',
     name: 'Family Card',
     icon: '/icon_service_family_card.svg'
   },
-  {
-    id: 'business-card',
-    name: 'Business Card',
-    icon: '/icon_service_business_card.svg'
-  },
-  {
-    id: 'ficha-familia',
-    name: 'Ficha Familia',
-    icon: '/icon_service_ficha_familia.svg'
-  },
+  // {
+  //   id: 'business-card',
+  //   name: 'Business Card',
+  //   icon: '/icon_service_business_card.svg'
+  // },
+  // {
+  //   id: 'ficha-familia',
+  //   name: 'Ficha Familia',
+  //   icon: '/icon_service_ficha_familia.svg'
+  // },
   {
     id: 'citizen-card',
     name: 'Citizen Card',
