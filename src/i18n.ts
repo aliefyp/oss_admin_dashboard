58222c3d79286fb840019b97374eb9136c8fb295
Alief Yoga Priyanto <aliefyp@gmail.com>
2024-04-23 10:15:32 +0700

    fix: rename lang code in config

--- src/i18n.ts
+++ src/i18n.ts
@@ -8,7 +8,7 @@ i18n
   // .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    lng: "tm",
+    lng: "tl",
     fallbackLng: "en",
     // debug: process.env.NODE_ENV === "development",
     react: {
