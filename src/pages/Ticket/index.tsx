dafbcad91a33b59bf5916911b7e2255e5785c42e
Alief Yoga Priyanto <aliefyp@gmail.com>
2024-08-05 19:45:43 +0700

    fix: remove pills on ticket

--- src/pages/Ticket/index.tsx
+++ src/pages/Ticket/index.tsx
@@ -1,18 +1,15 @@
-import { Chip, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { useZendeskTickets } from "api/zendesk";
 import PageHeading from "components/PageHeading";
 import { useEffect, useState } from "react";
-import useAuthUser from "react-auth-kit/hooks/useAuthUser";
 import { useTranslation } from "react-i18next";
 import { useLocation, useNavigate } from "react-router-dom";
-import { UserData } from "types/auth/user";
 import TicketTable from "./components/TicketTable";
 
 const Applicants: React.FC = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const { t } = useTranslation();
-  const auth = useAuthUser<UserData>();
 
   const [paginationModel, setPaginationModel] = useState({
     pageSize: 10,
@@ -39,24 +36,10 @@ const Applicants: React.FC = () => {
       <PageHeading title={t('page_ticket.title')} />
       <div className="space-y-4">
         <div className="mb-6 space-y-3">
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 flex-wrap">
             <Typography variant="caption" className="text-gray-600 block">
               <span dangerouslySetInnerHTML={{ __html: t('page_ticket.total_registered', { count: dataTickets?.metadata?.totalCount }) }} />
             </Typography>
-            {auth.serviceTypes?.map(service => (
-              <Chip
-                size="small"
-                variant="outlined"
-                label={t(`services.${service.name}`)}
-              />
-            ))}
-            {auth.regions?.map(region => (
-              <Chip
-                size="small"
-                variant="outlined"
-                label={region.name}
-              />
-            ))}
           </div>
         </div>
         <div style={{ width: '100%' }}>
